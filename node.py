9cc41495278108b6fee965e560e9be3fca923686
Carlo Keizer <ck.soest@hotmail.com>
2020-07-01 16:54:06 +0200

    1.0 Final

--- node.py
+++ node.py
@@ -10,6 +10,18 @@ class Node:
         self.propose_value = None
         self.promise_received = 0
         self.accepted_received = 0
+        self.rejected_received = 0
+        self.reject_promise = []
+        self.reject_accepted = []
+        self.reject_rejected = []
+
+    def to_fail(self, tick):
+        self.failed = True
+        print(str(tick) + " " + str(self) + " failed")
+
+    def to_recover(self, tick):
+        self.failed = False
+        print(str(tick) + " " + str(self) + " recovered")
 
     def __str__(self):
         return self.node_id
--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ from event import Event
 
 class Simulation:
 
-    def __init__(self, nP, nA, tmax):
+    def __init__(self, nP, nA, nL, tmax):
         if nP + nA > 10:
             raise Exception("maximaal 10 nodes")
         if tmax > 999:
@@ -16,43 +16,43 @@ class Simulation:
         self.propose_id = 1
         self.proposals = [Node("P", i) for i in range(nP)]
         self.acceptors = [Node("A", i) for i in range(nP, nP+nA)]
+        self.learners = [Node("L", i) for i in range(nP+nA, nP+nA+nL)]
         self.tmax = tmax
 
     def set_events(self):
-        self.events.append(Event(0, [], [], self.proposals[0], 42))
-        self.events.append(Event(3, [], [], self.proposals[1], 400))
-        self.events.append(Event(17, [], [], self.proposals[0], 1000))
+        self.events.append(Event(0, [], [], self.proposals[0], 43))
+        # self.events.append(Event(1, [], [], self.proposals[0], 44))
+        self.events.append(Event(1, [], [], self.proposals[1], 45))
+        # self.events.append(Event(7, self.acceptors[:2], [], None, None))
+        # self.events.append(Event(15, [], self.acceptors[:2], self.proposals[0], None))
 
     def run(self):
         for i in range(self.tmax):
-            ran_event = False
             for event in self.events:
                 if event.tick == i:
                     for node in event.failed:
-                        node.failed = True
+                        node.to_fail(i)
                     for node in event.recovered:
-                        node.failed = False
+                        node.to_recover(i)
                     if (event.proposal is not None) and (event.value is not None):
                         propose_message = Message(i, None, event.proposal, "PROPOSE", value=event.value)
                         propose_message.send(self.queue)
                     self.events.remove(event)
-                    ran_event = True
 
-            if not ran_event:
-                for proposal in self.proposals:
+            for proposal in self.proposals:
+                if not proposal.failed:
                     for message in self.network:
                         if message.dst == proposal:
 
-                            if message.message_type == "PROPOSE" and proposal.state == "SLEEP":
+                            if message.message_type == "PROPOSE":
                                 for acceptor in self.acceptors:
                                     prepare_message = Message(i, proposal, acceptor, "PREPARE", propose_id=self.propose_id)
                                     prepare_message.send(self.queue)
                                 proposal.propose_value = message.value = message.value
                                 proposal.propose_id = self.propose_id
                                 self.propose_id += 1
-                                proposal.state = "ACTIVE"
 
-                            if message.message_type == "PROMISE":
+                            if message.message_type == "PROMISE" and message.propose_id not in proposal.reject_promise:
                                 if message.propose_id == proposal.propose_id:
                                     proposal.promise_received += 1
                                     if proposal.promise_received > len(self.acceptors)/2:
@@ -60,43 +60,63 @@ class Simulation:
                                             accept_message = Message(i, proposal, acceptor, "ACCEPT", propose_id=message.propose_id, value=proposal.propose_value)
                                             accept_message.send(self.queue)
                                         proposal.promise_received = 0
-                                        for m in self.network:
-                                            if m.message_type == "PROMISE" and m.dst == proposal and m.propose_id == proposal.propose_id:
-                                                if m != message:
-                                                    self.network.remove(m)
+                                        proposal.reject_promise.append(message.propose_id)
 
-                            if message.message_type == "ACCEPTED":
+                            if message.message_type == "ACCEPTED" and message.propose_id not in proposal.reject_accepted:
                                 if message.propose_id == proposal.propose_id and message.value == proposal.propose_value:
                                     proposal.accepted_received += 1
                                     if proposal.accepted_received > len(self.acceptors)/2:
+                                        for learner in self.learners:
+                                            success_message = Message(i, proposal, learner, "SUCCESS", value=message.value)
+                                            success_message.send(self.queue)
                                         proposal.accepted_received = 0
                                         proposal.propose_value = None
-                                        proposal.propose_id = None
-                                        proposal.state = "SLEEP"
-                                        for m in self.network:
-                                            if m.message_type == "ACCEPTED" and m.dst == proposal and m.propose_id == proposal.propose_id:
-                                                if m != message:
-                                                    self.network.remove(m)
+                                        proposal.propose_id = 0
+                                        proposal.reject_accepted.append(message.propose_id)
+
+                            if message.message_type == "REJECTED" and message.propose_id not in proposal.reject_rejected:
+                                if message.propose_id == proposal.propose_id:
+                                    proposal.rejected_received += 1
+                                    if proposal.rejected_received > len(self.acceptors)/2:
+                                        for acceptor in self.acceptors:
+                                            prepare_message = Message(i, proposal, acceptor, "PREPARE", propose_id=self.propose_id)
+                                            prepare_message.send(self.queue)
+                                        proposal.propose_id = self.propose_id
+                                        self.propose_id += 1
+                                        proposal.rejected_received = 0
+                                        proposal.reject_rejected.append(message.propose_id)
 
                             self.network.remove(message)
 
-                for acceptor in self.acceptors:
+            for acceptor in self.acceptors:
+                if not acceptor.failed:
                     for message in self.network:
                         if message.dst == acceptor:
 
-                            if message.message_type == "PREPARE" and acceptor.state == "SLEEP":
+                            if message.message_type == "PREPARE":
                                 if message.propose_id > acceptor.promised:
                                     acceptor.promised = message.propose_id
                                     promise_message = Message(i, acceptor, message.src, "PROMISE", propose_id=message.propose_id)
                                     promise_message.send(self.queue)
-                                    acceptor.state = "ACTIVE"
 
                             if message.message_type == "ACCEPT":
                                 if message.propose_id == acceptor.promised:
                                     acceptor.records.append({"propose_id": message.propose_id, "value": message.value})
                                     accepted_message = Message(i, acceptor, message.src, "ACCEPTED", propose_id=message.propose_id, value=message.value)
                                     accepted_message.send(self.queue)
-                                    acceptor.state = "SLEEP"
+                                else:
+                                    rejected_message = Message(i, acceptor, message.src, "REJECTED", propose_id=message.propose_id)
+                                    rejected_message.send(self.queue)
+
+                            self.network.remove(message)
+
+            for learner in self.learners:
+                if not learner.failed:
+                    for message in self.network:
+                        if message.dst == learner:
+
+                            if message.message_type == "SUCCESS":
+                                print(str(i) + " " + str(learner) + " PREDICTED " + str(message.value*1.11))
 
                             self.network.remove(message)
 
@@ -104,7 +124,7 @@ class Simulation:
             self.queue = []
 
 
-a = Simulation(2, 5, 150)
+a = Simulation(2, 3, 1, 150)
 a.set_events()
 a.run()
 print(a.network)
